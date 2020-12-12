903b18e171318a79e10bd48cd93b68515de1408e
MichaelTseng <yucheng0939@gmail.com>
2020-12-12 22:06:24 +0800

    Initial Commit

---
+++ swift_ios_alignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_ios_alignment
+//
+//  Created by yuchengtseng on 2020/12/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
